e6b2ec71be17ff7a8946512a408036aeea6b0e7d
Vinod <vinod.awar@gmail.com>
2023-08-19 13:34:20 +0530

    Adapter pattern demo

---
+++ src/main/java/com/org/examples/patterns/AdvancedMediaPlayer.java
@@ -0,0 +1,7 @@
+package com.org.examples.patterns;
+
+public interface AdvancedMediaPlayer {
+ 
+	public void playVlc(String fileName);
+	   public void playMp4(String fileName);
+}
---
+++ src/main/java/com/org/examples/patterns/MediaPlayer.java
@@ -0,0 +1,5 @@
+package com.org.examples.patterns;
+
+public interface MediaPlayer {
+	public void play(String audioType, String fileName);
+}
